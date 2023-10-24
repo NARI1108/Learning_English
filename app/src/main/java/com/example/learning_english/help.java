0390d01b7527daf392e0cf6fddf7163c7a73e13f
Hill <125409221+NARI1108@users.noreply.github.com>
2023-10-24 22:22:06 +0330

    Addition of a custom toolbar for help activity.

---
+++ app/src/main/java/com/example/learning_english/help.java
@@ -0,0 +1,13 @@
+package com.example.learning_english;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class help extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
